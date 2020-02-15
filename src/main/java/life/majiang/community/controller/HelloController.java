58e0ddde35396a6f827a6ce679af30e2f39de132
wangkai_global <1805678859@qq.com>
2020-02-15 17:42:00 +0800

    init repo

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 王凯
+ * @version 1.0
+ * @date 2020/2/15 11:14
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
